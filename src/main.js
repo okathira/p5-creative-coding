19faba246d7927609e3b7a8ee4b32fb6d32a84d3
okathira <43328895+okathira@users.noreply.github.com>
2022-03-14 19:06:58 +0900

    Fix some 𝓛𝓸𝓿𝓮𝓼...

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@
 
 // heart bubble
 // reference: p5.js　バブルアップ - techtyの日記 https://techty.hatenablog.com/entry/2019/05/07/164956
+
 class HeartBubble {
-  static SIZE_MAX = 85;
+  static SIZE_MAX = 90;
   static SIZE_MIN = 20;
-  static SIZE_NUM = 30;
+  static SIZE_NUM = 40;
   static TRANSPERENT = 192;
   static COLOR = [
     [255, 255, 127, HeartBubble.TRANSPERENT],
@@ -15,7 +16,14 @@ class HeartBubble {
   ];
 
   constructor() {
-    this.size = random(HeartBubble.SIZE_MIN, HeartBubble.SIZE_MAX);
+    this.size = map(
+      randomGaussian(),
+      -1.5,
+      3,
+      HeartBubble.SIZE_MIN,
+      HeartBubble.SIZE_MAX,
+      true
+    );
     this.color = int(random(0, HeartBubble.COLOR.length));
     this.x = random(0, width);
     this.y = random(0, height + this.size);
@@ -37,7 +45,14 @@ class HeartBubble {
 
     if (this.y < 0 - this.size) {
       // 上がりきったら
-      this.size = random(HeartBubble.SIZE_MIN, HeartBubble.SIZE_MAX);
+      this.size = map(
+        randomGaussian(),
+        -1.5,
+        3,
+        HeartBubble.SIZE_MIN,
+        HeartBubble.SIZE_MAX,
+        true
+      );
       this.color = int(random(0, HeartBubble.COLOR.length));
       this.x = random(0, width);
       this.y = height + this.size;
@@ -47,7 +62,6 @@ class HeartBubble {
   }
 
   draw() {
-    strokeWeight(0);
     fill(HeartBubble.COLOR[this.color]);
     textSize(this.size);
     text('♥', this.x, this.y);
@@ -57,24 +71,38 @@ class HeartBubble {
 /** @type {HeartBubble[]} */
 const hearts = [];
 
-function setup() {
-  frameRate(60);
-  createCanvas(720, 540);
-
+const initHearts = () => {
   for (let i = 0; i < HeartBubble.SIZE_NUM; i++) {
     hearts.push(new HeartBubble());
   }
+};
+
+const drawHearts = () => {
+  push();
 
   drawingContext.shadowInset = true;
   drawingContext.shadowBlur = 8;
   drawingContext.shadowColor = '#fff8';
-}
-
-function draw() {
-  background(60, 120, 180);
+  textAlign(CENTER, CENTER);
+  strokeWeight(0);
 
   hearts.forEach((heart) => {
     heart.update();
     heart.draw();
   });
+
+  pop();
+};
+
+function setup() {
+  frameRate(60);
+  createCanvas(720, 540);
+
+  initHearts();
+}
+
+function draw() {
+  background(60, 120, 180);
+
+  drawHearts();
 }

e279af2e632eb25000a0703a2f6fb90e37cc4593
okathira <43328895+okathira@users.noreply.github.com>
2023-09-09 17:05:08 +0900

    パラメータの配列でアニメーションできるように

--- src/main.js
+++ src/main.js
@@ -2,81 +2,67 @@
  * @typedef { import("p5").Font } Font
  *
  * @typedef {{
- *   chr: string
- *   x:number
- *   y:number
- * }} Letter
- *
- * @typedef {(text: string, t: number) => Letter[]} GetLetters
- *
- * @typedef {(t: number) => void} AnimLine
- *
- * @typedef {{
  *  chrs: string
  *  entryStep: number
- *  getLetters: GetLetters
- *  animLines: AnimLine[]
+ *  letterAnimParamsList: AnimParam[][]
+ *  lineAnimParams: AnimParam[]
  * }} TextLine
+ *
+ * @typedef {Object} AnimParam
+ * @property {number=} x
+ * @property {number=} y
+ * @property {number=} rot
+ * @property {number=} rotX
+ * @property {number=} rotY
+ * @property {EasingFuncName | EasingNoneName} easing
  */
 
-const TEMPO = 20;
+const TEMPO = 17.5;
 
 /**
- * @type {GetLetters}
+ * @typedef {(t: number) => number} EasingFunc
+ * @typedef {'easeOutCubic' | 'easeLinear'} EasingFuncName
+ * @typedef {'none'} EasingNoneName
  */
-const getLettersFunc = (chrs, t) => {
-  const chrArray = [...chrs];
-  const chrNum = chrArray.length;
-  const spacingX = 50;
-  const centeringX = (spacingX * (chrNum - 1)) / 2;
-  return chrArray.map((chr, i) => ({
-    chr,
-    x: spacingX * i - centeringX,
-    y: (() => {
-      switch (i) {
-        case 1:
-          return 50 * t;
-        case 2:
-          return 15 * t;
-        default:
-          return 0;
-      }
-    })(), //t * i,
-  }));
-};
 
 /**
- * @type {(t: number) => number}
+ * @type {EasingFunc}
  */
 const easeOutCubic = (t) => 1 - (1 - t) * (1 - t) * (1 - t);
 
 /**
- * @type {(t: number) => number}
+ * @type {EasingFunc}
  */
-const limit = (t) => map(t, 0, 1, 0, 1, true);
+const easeLinear = (t) => t;
 
 /**
- * @type {AnimLine}
+ * @type {Record<EasingFuncName, EasingFunc> & Record<EasingNoneName, undefined> }
  */
-const animLineFunc1 = (t) => {
-  const x = -100;
-  const y = 0;
-  translate(x, y);
-  rotateZ(10 * easeOutCubic(t));
-
-  translate(-x, -y);
+const easingFuncList = {
+  none: undefined,
+  easeLinear,
+  easeOutCubic,
 };
 
+// animLineFunc をパラメータで機能するように
 /**
- * @type {AnimLine}
+ * @type {(t: number, param: AnimParam) => void}
  */
-const animLineFunc2 = (t) => {
-  const x = -100;
-  const y = 50;
-  translate(x, y);
-  rotateZ(-30 * easeOutCubic(t));
+const animLineFunc = (
+  t,
+  { x = 0, y = 0, rot = 0, rotX = 0, rotY = 0, easing }
+) => {
+  if (easing === 'none') return;
 
-  translate(-x, -y);
+  const easedTime = easingFuncList[easing](t);
+
+  // 理論上は回転(rot)と平行移動(x, y)だけで表現できるが、利便性のために回転軸座標(rotX, rotY)ももたせる
+  // translate(), rotate()が重そうなら、回転と平行移動のみに変換してぞれぞれ合計していくことで最後にまとめて処理できるはず
+  translate(rotX, rotY);
+  rotateZ(rot * easedTime);
+  translate(-rotX, -rotY);
+
+  translate(x * easedTime, y * easedTime);
 };
 
 /**
@@ -86,8 +72,16 @@ const textLines = [
   {
     chrs: '<私は>',
     entryStep: 0,
-    getLetters: getLettersFunc,
-    animLines: [animLineFunc1, animLineFunc2],
+    letterAnimParamsList: [
+      [{ easing: 'none' }],
+      [{ easing: 'easeLinear', y: 40 }],
+      [{ easing: 'easeLinear', y: 10 }],
+      [{ easing: 'none' }],
+    ],
+    lineAnimParams: [
+      { rot: 10, rotX: -100, easing: 'easeOutCubic' },
+      { rot: -30, rotX: -100, rotY: 50, easing: 'easeOutCubic' },
+    ],
   },
 ];
 
@@ -112,40 +106,66 @@ function setup() {
   angleMode(DEGREES);
 }
 
-let time = 0;
+let frame = 0;
 function draw() {
   if (mouseIsPressed) {
-    time = 0;
+    frame = 0;
   }
 
   background('#501010');
 
   textLines.map((line) => {
-    const { chrs, entryStep, getLetters, animLines } = line;
-    const t = time / TEMPO;
-
-    const letters = getLetters(chrs, limit(t));
+    const { chrs, entryStep, letterAnimParamsList, lineAnimParams } = line;
+    const t = frame / TEMPO;
 
     push();
 
-    animLines.map((animLine, i) => {
-      const step = int(t - entryStep);
+    const step = Math.trunc(t - entryStep);
 
+    // 文字の行にかかるアニメーション
+    lineAnimParams.map((lineAnimParam, i) => {
       if (i < step) {
-        animLine(1);
+        animLineFunc(1, lineAnimParam);
       } else if (i === step) {
-        animLine(t % 1);
+        animLineFunc(t % 1, lineAnimParam);
       }
     });
 
-    letters.map((letters) => {
-      const { chr, x, y } = letters;
+    // const letters = getLetters(chrs, limit(t));
 
-      text(chr, x, y);
+    // TODO: 関数にする 中央から広がるパターンと左からスライドするパターンなども
+    // それぞれの文字の座標を計算
+    const chrArray = [...chrs];
+    const chrNum = chrArray.length;
+    const spacingX = 50;
+    const centeringX = (spacingX * (chrNum - 1)) / 2;
+
+    if (chrNum !== letterAnimParamsList.length)
+      throw new Error('chrNum !== letterAnimParamsList.length');
+
+    chrArray.map((chr, i) => {
+      push();
+
+      // 行の文字それぞれにかかるアニメーション
+      translate(spacingX * i - centeringX, 0);
+
+      // それぞれの文字が個別に持つアニメーション
+      const letterAnimParams = letterAnimParamsList[i];
+      letterAnimParams.map((letterAnimParam, i) => {
+        if (i < step) {
+          animLineFunc(1, letterAnimParam);
+        } else if (i === step) {
+          animLineFunc(t % 1, letterAnimParam);
+        }
+      });
+
+      text(chr, 0, 0);
+
+      pop();
     });
 
     pop();
   });
 
-  time += 1;
+  frame += 1;
 }

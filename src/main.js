62e63a2aa8492dc6407518ea05b74c0fbcc65d18
okathira <43328895+okathira@users.noreply.github.com>
2023-09-11 02:22:09 +0900

    三拍目追加

--- src/main.js
+++ src/main.js
@@ -79,7 +79,7 @@ const animLineFunc = (t, param) => {
  */
 const textLines = [
   {
-    chrs: '<私は>',
+    chrs: '＜私は＞',
     entry: { step: 0, x: 0, y: 0, rot: 0 },
     letterAnimParamsList: [
       [{ easing: 'none' }],
@@ -89,18 +89,31 @@ const textLines = [
     ],
     lineAnimParams: [
       { rot: 10, rotX: -100, easing: 'easeOutCubic' },
-      { rot: -30, rotX: -100, rotY: 50, easing: 'easeOutCubic' },
+      { rot: -35, rotX: -100, rotY: 40, easing: 'easeOutCubic' },
+      { rot: +35, rotX: 100, rotY: 40, easing: 'easeOutCubic' },
     ],
   },
   {
     chrs: '集積の',
-    entry: { step: 1, x: 60, y: 60, rot: 0 },
+    entry: { step: 1, x: 80, y: 80, rot: 0 },
     letterAnimParamsList: [
       [{ easing: 'easeLinear', y: 40 }],
       [{ easing: 'none' }],
       [{ easing: 'none' }],
     ],
-    lineAnimParams: [{ rot: 10, rotX: 150, easing: 'easeOutCubic' }],
+    lineAnimParams: [
+      { rot: 20, rotX: 100, easing: 'easeOutCubic' },
+      { rot: -25, rotX: -150, rotY: 100, easing: 'easeOutCubic' },
+    ],
+  },
+  {
+    chrs: '海を',
+    entry: { step: 2, x: 60, y: 150, rot: 10 },
+    letterAnimParamsList: [
+      [{ easing: 'easeLinear', y: 20 }],
+      [{ easing: 'none' }],
+    ],
+    lineAnimParams: [{ rot: -15, x: -50, y: -80, easing: 'easeOutCubic' }],
   },
 ];
 
@@ -125,7 +138,7 @@ function setup() {
   background(127);
   textFont(font);
   textAlign(CENTER, CENTER);
-  textSize(20);
+  textSize(18);
 
   pElement = createP('info text');
   pElement.position(0, 0);

22bde92942fbd1508227c13955ed7e4f790f3e53
Takahiro <43328895+okathira@users.noreply.github.com>
2023-09-19 04:38:33 +0900

    2dg merge (#2)
    * 編集用テンプレブランチ作成
    
    * 𝓛𝓸𝓿𝓮...
    
    * Fix some 𝓛𝓸𝓿𝓮𝓼...
    
    * タイトルを大まかに作成
    
    * 光り方修正
    
    * 虹を削って文字を調整
    
    * 画像を表示する
    
    画像素材は同梱しない
    
    * 見栄え修正
    
    * 虹色は文字が動いても変わらないように
    
    * 文字盤を回す
    
    * CSSつける
    
    loadFontがうまくいかないので初回リロード必須
    
    * フォント表示が適切に表示されるようにする
    
    * ページ自体の手直し
    
    * タイポ修正
    
    * 30FPSに合わせた
    
    * update p5
    
    * 処理を関数にまとめる
    
    * タイトルの描画を寄せる
    
    * 中央に円形画像を配置する
    
    * 画像上のテキストを表示する
    
    * 動作が重い場所に検討をつける
    
    * キャンバスを保存できるようにする
    
    * getImageData()を使わないように処理を変更する
    
    * 設定パラメータを上にもってくる
    
    * 透過画像に対応する
    
    * プロジェクトをマージ

--- src/terrain-grid/main.js
+++ src/terrain-grid/main.js
@@ -1,69 +1,42 @@
-const SIZE_X = 40;
-const SIZE_Y = 30;
-const w = 1000;
-const h = 1200;
-const cols = Math.floor(w / SIZE_X);
-const rows = Math.floor(h / SIZE_Y);
-
-const NOISE_SCALE = 0.125;
-
-/** @type {number[][]} */
-let terrain = Array.from({ length: rows + 1 }, () => Array(cols).fill(0));
-let wave = 0;
+const GRID_NUM = 20;
+const w = 1200;
+const h = 1000;
+const GRID_X = w / GRID_NUM;
+const GRID_Y = h / GRID_NUM;
 
 function setup() {
   // createCanvas(windowWidth, windowHeight, WEBGL);
   createCanvas(640, 640, WEBGL);
   fill(0, 0, 64);
-  stroke(220, 60, 200);
+  stroke(255);
 }
 
-const refreshTerrain = () => {
-  wave += 0.15;
-  for (let y = 0; y < rows + 1; y++) {
-    for (let x = 0; x < cols; x++) {
-      terrain[y][x] = map(
-        noise((x - wave) * NOISE_SCALE, y * NOISE_SCALE),
-        0,
-        1,
-        -150,
-        150
-      );
-    }
-  }
-};
-
-// 三角を六角形に見えるように並べる（四角形ではなく）
-const drawGrid = (/** @type {number} */ x, /** @type {number} */ y) => {
-  if (y % 2) {
-    const adjTop = -0.25;
-    const adjBtm = 0.25;
-    vertex((x + adjTop) * SIZE_X, y * SIZE_Y, terrain[y][x]);
-    vertex((x + adjBtm) * SIZE_X, (y + 1) * SIZE_Y, terrain[y + 1][x]);
-  } else {
-    const adjTop = 0.25;
-    const adjBtm = -0.25;
-    vertex((x + adjBtm) * SIZE_X, (y + 1) * SIZE_Y, terrain[y + 1][x]);
-    vertex((x + adjTop) * SIZE_X, y * SIZE_Y, terrain[y][x]);
-  }
-};
-
-const drawTerrain = () => {
-  for (let y = 0; y < rows; y++) {
-    beginShape(TRIANGLE_STRIP);
-    for (let x = 0; x < cols; x++) {
-      drawGrid(x, y);
-    }
-    endShape();
-  }
+const drawGrid = (
+  /** @type {number} */ x,
+  /** @type {number} */ y,
+  /** @type {number} */ adjTop = 0,
+  /** @type {number} */ adjBtm = 0
+) => {
+  vertex((x + adjTop) * GRID_X, y * GRID_Y);
+  vertex((x + adjBtm) * GRID_X, (y + 1) * GRID_Y);
+  // vertex((x + 1 + adjTop) * GRID_X, y * GRID_Y);
 };
 
 function draw() {
-  background(180, 100, 160);
-
+  background(0);
   rotateX((PI / 180) * 60);
-  rotateZ(PI / 2);
   translate(-w / 2, -h / 2);
-  refreshTerrain();
-  drawTerrain();
+
+  // terrain triangles
+  for (let y = 0; y < GRID_NUM; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (let x = 0; x < GRID_NUM; x++) {
+      if (y % 2) {
+        drawGrid(x, y, -0.25, 0.25);
+      } else {
+        drawGrid(x, y, 0.25, 0.75);
+      }
+    }
+    endShape();
+  }
 }
